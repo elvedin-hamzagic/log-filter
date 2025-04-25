553ecaa9c7fa8ce7fe606d1e43cb55b850169ea1
Elvedin Hamzagic <elvedin.hamzagic@gmail.com>
2025-04-26 01:15:48 +0200

    v1.1.0

---
+++ Changelog.md
@@ -0,0 +1,7 @@
+1.0.0 - 2025-02-14
+- Initial release.
+
+1.1.0 - 2025-04-26
+- FIX: Problem with character encoding, which caused inconsistency in different terminals
+- MOD: Handling regex
+- MOD: Extended instruction text
--- src/main/java/com/codeinspace/Main.java
+++ src/main/java/com/codeinspace/Main.java
@@ -1,7 +1,9 @@
 package com.codeinspace;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
@@ -9,14 +11,15 @@ public class Main {
     private static boolean filterLogFile(String inputFilePath, String outputFilePath, String includeFilter) {
         try {
             BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(inputFilePath));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
             BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(outputFilePath));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
 
+            Pattern pattern = Pattern.compile(includeFilter);
             while (reader.ready()) {
                 String line = reader.readLine();
-                if (line.matches(includeFilter)) {
+                if (pattern.matcher(line).matches()) {
                     writer.write(line);
                     writer.newLine();
                 }
@@ -33,8 +36,14 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Usage: java -jar log-filter.jar <path-to-input-file> " +
-                    "-o<path-to-output-file> -r\"include-filter-regex\"");
+            System.out.println("\nUsage: java -jar log-filter.jar <path-to-input-file> " +
+                    "-o<path-to-output-file> -r\"include-filter-regex\"\n");
+
+            System.out.println("NOTE:");
+            System.out.println("Some terminals cannot handle arguments which contains double quotes," +
+                    "in that case you can use the escape sequence \\u0022.");
+            System.out.println("Some terminals will not pass non-ascii characters in arguments correctly," +
+                    "in that case you can use the character unicode escape sequence.\n");
             return;
         }
 
@@ -49,6 +58,8 @@ public class Main {
                 regexFilter = args[i].substring(2);
             }
         }
+
+        System.out.println("Filtering log file using regex: " + regexFilter);
 
         boolean success = filterLogFile(inputFilePath, outputFilePath, regexFilter);
         if (!success) { LOGGER.severe("Failed to filter log file."); return; }
